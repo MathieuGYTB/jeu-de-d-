f2a55214e1b09c09f0184e89f2d14b71f8450205
MathieuGYTB <mat.68@orange.fr>
2022-05-26 16:46:00 +0200

    add new animation

--- js/rollDiceClickFunction.mjs
+++ js/rollDiceClickFunction.mjs
@@ -1,5 +1,5 @@
 import './three.mjs'
-import {animate, stopAnime} from './three.mjs'
+import {box} from './three.mjs'
 // jquery
 $(document).ready(() => {
   console.log('jQuery est prêt à l\'utilisation')
@@ -15,15 +15,71 @@ $(document).ready(() => {
   //const to add #rollDice element
   const rollDice = $('#rollDice');
 
+var number;
+  
   // fonction to add random number between 1 and 6 after 5 sec
-  function addRandomNumber() {
-    setTimeout(() => {
-      let number = Number(Math.ceil(Math.random()*6))
+  function AddRandomNumber() {
+       number = Number(Math.floor(Math.random()*6) + 1)
       console.log(number)
-    }, 5000);
+  };
+  //function to move dice
+  function MoveDice() {
+    AddRandomNumber();
+    if (number === 1) {
+      //player 2
+      // move dice on face 1
+      setTimeout(() => {
+        box.rotation.y -= 90;
+      },4000);
+      setTimeout(() => {
+        box.rotation.y += 90
+      }, 6000);
+    } else if (number === 2) {
+      //add temporary count
+      // move dice on face 2
+     setTimeout(()=> {
+      box.rotation.y += 179;
+     }, 4000);
+     setTimeout(() => {
+      box.rotation.y -= 179
+    }, 6000);
+    } else if (number === 3) {
+      //add temporary count
+      // move dice on face 3
+     setTimeout(() => {
+       box.rotation.x += 90;
+     }, 4000);
+     setTimeout(() => {
+      box.rotation.x -= 90
+    }, 6000); 
+    } else if (number === 4) {
+      //add temporary count
+      //move dice on face 4
+      setTimeout(() => {
+        box.rotation.x -= 90;
+      }, 4000);
+      setTimeout(() => {
+        box.rotation.x += 90
+      }, 6000);
+    } else if (number === 5) {
+      //add temporary count
+      //move dice on face 5
+      box.rotation.x == 0;
+    } else if (number === 6) {
+      //add temprary count
+      //move dice on face 6
+     setTimeout(() => {
+       box.rotation.y += 90;
+     }, 4000);
+     setTimeout(() => {
+      box.rotation.y -= 90
+    }, 6000);
+    } else {
+      console.log('error')
+    }
   };
 
   // to add event on click on rollDice element
-  rollDice.click(addRandomNumber).click(playSound).click(animate).click(stopAnime);
+  rollDice.click(playSound).click(MoveDice);
   
   });
--- js/three.mjs
+++ js/three.mjs
@@ -4,62 +4,46 @@ import * as THREE from "https://unpkg.com/three@0.140.2/build/three.module.js";
 
 
   //get element canvas
-  export const canvas1 = document.getElementsByTagName('canvas');
+export const canvas1 = document.getElementsByTagName('canvas');
 
   //add context 
-  export const gl = canvas1[0].getContext('webgl');
+export const gl = canvas1[0].getContext('webgl');
   //add scene, camera and renderer
-  export const scene = new THREE.Scene();
-  export const camera = new THREE.PerspectiveCamera(75, canvas1[0].width / canvas1[0].height, 0.1, 1000);
-  camera.position.z = 3;
-  export const renderer = new THREE.WebGLRenderer(
-    {
-    canvas: canvas1[0],
-    alpha: true
-    }
-  );
+export const scene = new THREE.Scene();
+export const camera = new THREE.PerspectiveCamera(75, canvas1[0].width / canvas1[0].height, 0.1, 1000);
+camera.position.z = 3;
+export const renderer = new THREE.WebGLRenderer(
+  {
+  canvas: canvas1[0],
+  alpha: true
+  }
+);
  
 
   // add box object
-  export const boxGeometry = new THREE.BoxGeometry(2,2,2);
-  export const boxMaterial = 
-    [
-      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader( ).load('../images/dice1.png'), side: THREE.DoubleSide }),
-      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader( ).load('../images/dice2.png'), side: THREE.DoubleSide }),
-      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader( ).load('../images/dice3.png'), side: THREE.DoubleSide }),
-      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader( ).load('../images/dice4.png'), side: THREE.DoubleSide }),
-      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader( ).load('../images/dice5.png'), side: THREE.DoubleSide }),
-      new THREE.MeshBasicMaterial({map: new THREE.TextureLoader( ).load('../images/dice6.png'), side: THREE.DoubleSide })
-    ];
+export const boxGeometry = new THREE.BoxGeometry(2,2,2);
+ export const boxMaterial = 
+  [
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader( ).load('../images/dice1.png'), side: THREE.DoubleSide }),
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader( ).load('../images/dice6.png'), side: THREE.DoubleSide }),
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader( ).load('../images/dice3.png'), side: THREE.DoubleSide }),
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader( ).load('../images/dice4.png'), side: THREE.DoubleSide }),
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader( ).load('../images/dice5.png'), side: THREE.DoubleSide }),
+    new THREE.MeshBasicMaterial({map: new THREE.TextureLoader( ).load('../images/dice2.png'), side: THREE.DoubleSide })
+  ];
 
-  export const material = new THREE.MeshFaceMaterial(boxMaterial);
-  export const box = new THREE.Mesh(boxGeometry, material);
-  box.position.set(0,0,0);
-  scene.add(box);
+export const material = new THREE.MeshFaceMaterial(boxMaterial);
+export const box = new THREE.Mesh(boxGeometry, material);
+box.position.set(0,0,0);
+scene.add(box);
 
 
   // add function to no animate dice
-  export function NoAnimate() {
-    box.position.set(0,0,0);
-    renderer.setClearColor(0xffffff, 0);
-    renderer.render(scene, camera);
-    requestAnimationFrame(NoAnimate)
-  };
- NoAnimate();
+export function NoAnimate() {
+  box.position.set(0,0,0);
+  renderer.setClearColor(0xffffff, 0);
+  renderer.render(scene, camera);
+  requestAnimationFrame(NoAnimate)
+};
+NoAnimate();
 
-  // function to animate the object
-  export const animate = function() {
-    
-      box.rotation.y += 0.1;
-      box.rotation.x += 0.1;
-      box.rotation.z += 0.01;
-      renderer.setClearColor( 0xffffff, 0 );
-      renderer.render(scene, camera);
-      requestAnimationFrame(animate);
-    
-  };
-  
-  // function to stop animation
-  export function stopAnime(){
-    setTimeout(NoAnimate, 5000);
-  }
